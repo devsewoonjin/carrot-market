448b12a9c6695ee193b546b80291a9c12e39a94d
devsewoonjin <dev.sewoonjin@gmail.com>
2024-04-07 14:43:58 +0900

    5.1 server action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,23 +3,34 @@ import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
+  const handleForm = async (formData: FormData) => {
+    "use server";
+    console.log(formData.get("email"), formData.get("password"));
+    console.log("i run in the server baby");
+  };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="text" placeholder="Email" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
+        <FormInput
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          errors={[]}
+        />
 
         <FormInput
+          name="password"
           type="password"
           placeholder=" Password"
           required
           errors={[]}
         />
-
-        <FormButton className="h-10">Create account</FormButton>
+        <FormButton className="h-10">Log in</FormButton>
       </form>
       <SocialLogin />
     </div>
